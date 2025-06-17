0396d5296fa599666b67bbc1b84580ba7446e154
prajwal240 <prajwal.dec7@gmail.com>
2025-06-17 15:29:08 +0530

    changed function for settlements

--- controllers/settlementsController.js
+++ controllers/settlementsController.js
@@ -29,31 +29,55 @@ exports.getBalances = async (req, res) => {
 };
 
 exports.getSettlements = async (req, res) => {
-  const { data: balances } = await exports.getBalances({ query: {} }, { json: x => x });
-  const owes = [], owed = [];
+  try {
+    const balances = {};
+    const expenses = await Expense.find();
 
-  for (const [person, balance] of Object.entries(balances)) {
-    if (balance < -0.01) owes.push({ person, amount: -balance });
-    else if (balance > 0.01) owed.push({ person, amount: balance });
-  }
+    expenses.forEach(exp => {
+      const splitAmount = exp.amount / exp.participants.length;
 
-  const settlements = [];
-  let i = 0, j = 0;
+      exp.participants.forEach(person => {
+        if (!balances[person]) balances[person] = 0;
+        balances[person] -= splitAmount;
+      });
 
-  while (i < owes.length && j < owed.length) {
-    const min = Math.min(owes[i].amount, owed[j].amount);
-    settlements.push({
-      from: owes[i].person,
-      to: owed[j].person,
-      amount: parseFloat(min.toFixed(2))
+      balances[exp.paid_by] = (balances[exp.paid_by] || 0) + exp.amount;
     });
 
-    owes[i].amount -= min;
-    owed[j].amount -= min;
+    Object.keys(balances).forEach(person => {
+      balances[person] = parseFloat(balances[person].toFixed(2));
+    });
 
-    if (owes[i].amount < 0.01) i++;
-    if (owed[j].amount < 0.01) j++;
+    const owes = [];
+    const owed = [];
+
+    for (const [person, balance] of Object.entries(balances)) {
+      if (balance < -0.01) owes.push({ person, amount: -balance });
+      else if (balance > 0.01) owed.push({ person, amount: balance });
+    }
+
+    const settlements = [];
+    let i = 0, j = 0;
+
+    while (i < owes.length && j < owed.length) {
+      const min = Math.min(owes[i].amount, owed[j].amount);
+
+      settlements.push({
+        from: owes[i].person,
+        to: owed[j].person,
+        amount: parseFloat(min.toFixed(2))
+      });
+
+      owes[i].amount -= min;
+      owed[j].amount -= min;
+
+      if (owes[i].amount < 0.01) i++;
+      if (owed[j].amount < 0.01) j++;
+    }
+
+    res.json({ success: true, data: settlements });
+  } catch (err) {
+    console.error("Error in getSettlements:", err);
+    res.status(500).json({ success: false, message: err.message });
   }
-
-  res.json({ success: true, data: settlements });
 };
